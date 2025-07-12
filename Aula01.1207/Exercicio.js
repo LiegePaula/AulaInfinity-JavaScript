e8167678a4a08584251adb3caa217edb6895dc2f
Liege de Paula <liegemfpaula@gmail.com>
2025-07-12 10:23:28 -0300

    Add files via upload

---
+++ Aula01.1207/Exercicio.js
@@ -0,0 +1,17 @@
+let nome = 'Liege'
+let sobrenome = 'Paula'
+const idade = 43
+let altura = 1.71
+let brasileira = true
+let nomeCompleto = nome+sobrenome
+
+nome = 'Rafael'
+idade = 44
+
+console.log(nome)
+console.log(sobrenome)
+console.log(idade)
+console.log(altura)
+console.log(brasileira)
+console.log(nome, sobrenome, idade)
+console.log(nomeCompleto)
---
+++ Aula01.1207/Exercicio2.js
@@ -0,0 +1,53 @@
+// exerciícios de variáveis:
+
+// 1 - Declare quatro variáveis e exiba seus valore no terminal.  
+let n1 = 70
+let n2 = 0.5
+let nome = 'Mariana'
+let frase = 'é monitora na Infinity'
+let chuva = false
+
+console.log(n1)
+console.log(n2)
+console.log(nome)
+console.log(frase)
+console.log(chuva)
+
+// 1.2 - Exiba os TIPOS das variáveis no terminal
+// exemplo de uso do "typeof" para checar tipo:
+let teste = 'Teste'
+console.log(typeof n1)
+console.log(typeof n2)
+console.log(typeof nome)
+console.log(typeof frase)
+console.log(typeof chuva)
+console.log(typeof teste)
+
+// 2 - Declarar duas variáveis do tipo STR e concatenar seus valores.
+
+let nome1 = 'Liege'
+let nome2 = "Paula"
+console.log(nome1+nome2)
+
+// 3 - Somar o valor de duas variáveis (numéricas)
+
+console.log(n1+n2)
+
+// 4 - Subtrair o valor de duas variáveis (numéricas)
+
+console.log(n1-n2)
+
+// 5 - Multiplicar o valor de duas variáveis númericas
+
+console.log(n1*n2)
+
+// 6 - Dividir o valor de duas variáveis numéricas
+
+console.log(n1/n2)
+let n3=50
+let n4=3
+console.log(n3/n4)
+
+// 7 - Obter o resto da divisão realizada no exercício anterior.
+
+console.log(n3%n4)
