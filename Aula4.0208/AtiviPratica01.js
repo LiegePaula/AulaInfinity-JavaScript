d3a6547d198df8c336e17e9b7eb2d21ab99d9838
LiegePaula <liegemfpaula@gmail.com>
2025-09-07 16:46:34 -0300

    atualização

---
+++ Aula4.0208/AtiviPratica01.js
@@ -0,0 +1,10 @@
+/* Atividade 01
+Primeiro, crie um array chamado cores contendo os seguintes elementos:
+Vermelho, Verde, Azul, Amarelo.
+Em seguida, acesse e exiba no console o elemento na posição 3.
+
+Objetivo:
+Praticar o uso de índices para acessar elementos específicos em um array. */
+
+let cores = [ 'vermelho', 'verde', 'azul', 'amarelo']
+console.log(cores[3])
