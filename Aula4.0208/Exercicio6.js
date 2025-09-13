781b2c3062800cab1dc5951242b299a184ddae49
Liege de Paula <liegemfpaula@gmail.com>
2025-09-13 11:52:54 -0300

    Delete Aula4.0208 directory

--- Aula4.0208/Exercicio6.js
+++
@@ -1,3 +0,0 @@
-// 6 - Crie um programa que realiza um filtro na lista abaixo, verificando se a palavra inicia ou termina com o carácter escolhido.
-let produtosDoMercado = ["aveia", "maçã", "uva", "abobora", "leite", "pão", "sabonete", "desodorante", "amaciante", "chuveiro"]
-
