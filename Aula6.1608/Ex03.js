a91f359e0d2311d1bfdcccc6025c1697b8264ac6
Liege de Paula <liegemfpaula@gmail.com>
2025-09-13 11:54:18 -0300

    Delete Aula6.1608 directory

--- Aula6.1608/Ex03.js
+++
@@ -1,7 +0,0 @@
-// 3 - Crie um programa que exiba a mesma mensagem porém com o seu nome.
-
-function saudacao(nome){
-    let mensagem = `Olá, ${nome}`
-    return mensagem
-}
-console.log(saudacao('Liege'))
